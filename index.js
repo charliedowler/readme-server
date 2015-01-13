aa520f34207676a97030dc092fab2f91221c8995
charliedowler <charliedowler15@gmail.com>
2015-01-13 23:59:25 +0000

    Load separate markdown files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var http = require('http');
 var marked = require('marked');
 var template = require('./template');
 
+var isMarkdown = /(.md|.markdown)$/i;
+
 var filename = fs.readdirSync('.').filter(function(name) {
   return /^readme(.md|.markdown)$/i.test(name);
 })[0];
@@ -14,33 +16,42 @@ if (!filename) {
 var contents = fs.readFileSync(filename);
 var changed = true;
 
-var server = http.createServer(function(req, resp) {
-  if (req.url !== '/') {
-    // Attempt to load images
-    try {
-      resp.end(fs.readFileSync('.' + req.url));
-    }
-    catch (e) {
-      resp.writeHead(404);
-      resp.end();
-    }
-    return true;
-  }
-  marked(contents.toString(), {
+var parser = function(txt, callback) {
+  marked(txt.toString(), {
     highlight: function (code) {
       return require('highlight.js').highlightAuto(code).value;
     }
   }, function (err, result) {
-    resp.setHeader('Content-Type', 'text/html');
-    result = template(result);
-    if (changed) {
-      changed = false;
+    callback(result);
+  });
+};
+
+var server = http.createServer(function(req, resp) {
+  try {
+    if (isMarkdown.test(req.url)) {
+      var isCurrentFile = new RegExp(filename, 'i').test(req.url);
+      var txt = isCurrentFile ? contents : fs.readFileSync('.' + req.url);
+      parser(txt, function(result) {
+        resp.setHeader('Content-Type', 'text/html');
+        result = template(result);
+        if (isCurrentFile && changed) {
+          changed = false;
+        }
+        else if (isCurrentFile) {
+          resp.writeHead(304);
+        }
+        resp.end(result);
+      });
     }
     else {
-      resp.writeHead(304);
+      resp.end(fs.readFileSync('.' + req.url));
     }
-    resp.end(result);
-  });
+  }
+  catch (e) {
+    resp.writeHead(404);
+    resp.end();
+  }
+  return true;
 });
 
 var spawn = require('child_process').spawn;
@@ -48,7 +59,7 @@ server.listen(5678, 'localhost', function(err) {
   if(err) {
     throw err;
   }
-  var host = ['http://localhost:' + server.address().port + '/'];
+  var host = ['http://localhost:' + server.address().port + '/' + filename];
   spawn('open', host);
   console.log('Server started on ' + host.join(''));
 });
