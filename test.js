664ab487f19d64b58bb334848abd6bac49b5d04e
charliedowler <charliedowler15@gmail.com>
2015-02-23 22:36:47 +0000

    Fixed tests

--- test.js
+++ test.js
@@ -3,22 +3,36 @@ var fs = require('fs');
 var http = require('http');
 var assert = require('assert');
 var marked = require('marked');
+var exec = require('child_process').exec;
 
 var file = fs.readFileSync('README.md');
 
-http.get('http://localhost:5678/README.md', function(res) {
-  var data = '';
-  res.on('data', function(chunk) {
-    data += chunk;
-  });
-  res.on('end', function() {
-    marked(file.toString(), {
-      highlight: function (code) {
-        return require('highlight.js').highlightAuto(code).value;
-      }
-    }, function (err, result) {
-      assert.equal(data.indexOf(result) > 0, true);
-      console.log('All tests passed'.green);
+exec('node index');
+
+setTimeout(function() {
+  http.get('http://localhost:5678/README.md', function(res) {
+    var data = '';
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+    res.on('end', function() {
+      marked(file.toString(), {
+        highlight: function (code) {
+          return require('highlight.js').highlightAuto(code).value;
+        }
+      }, function (err, result) {
+        try {
+          assert.equal(data.indexOf(result) > 0, true);
+        }
+        catch (e) {
+          console.log('Failure'.red);
+          console.log('From server:'.yellow, data);
+          console.log('Parsed markdown:'.yellow, result);
+          process.exit(1);
+        }
+        console.log('All tests passed'.green);
+        process.exit(0);
+      });
     });
   });
-});
+}, 1000);
