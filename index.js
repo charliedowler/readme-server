e92ca0d6f015e6df563245958a8f632f6eff5a5f
charliedowler <charliedowler15@gmail.com>
2015-03-05 23:30:04 +0000

    housekeeping

--- index.js
+++ index.js
@@ -1,115 +1,112 @@
 #!/usr/bin/env node
-'use strict';
-require('colors');
-var fs = require('fs');
-var http = require('http');
-var marked = require('marked');
-var template = require('./template');
-var exts = require('markdown-extensions');
+'use strict'
+require('colors')
+var fs = require('fs')
+var http = require('http')
+var marked = require('marked')
+var template = require('./template')
+var spawn = require('child_process').spawn
+var highlighter = require('highlight.js').highlightAuto
 
-var isMarkdown = new RegExp('(' + exts.join('|') + ')$', 'i');
+var markdownExtensions = '(md|markdown|mdown|mkdn|mkd|mdwn|mkdown|ron)$'
+var hasMarkdownExtension = new RegExp(markdownExtensions, 'i')
 
-var filename = fs.readdirSync('.').filter(function(name) {
-  return new RegExp('^readme.(' + exts.join('|') + ')$', 'i').test(name);
-})[0];
-if (!filename) {
-  throw new Error('Failed to find a readme file');
+var readmeFiles = fs.readdirSync('.').filter(function(name) {
+  return new RegExp('^readme.' + markdownExtensions, 'i').test(name)
+})
+var mainReadme = readmeFiles[0]
+if (!mainReadme) {
+  throw new Error('Failed to find a readme file')
 }
-var contents = fs.readFileSync(filename);
 
-var parser = function(txt, callback) {
-  marked(txt.toString(), {
+var parseMarkdown = function(txt, callback) {
+  marked(txt, {
     highlight: function (code) {
-      return require('highlight.js').highlightAuto(code).value;
+      return highlighter(code).value
     }
   }, function (err, result) {
-    callback(err, result);
-  });
-};
+    if (err) throw err
+    callback(result)
+  })
+}
 
 var getRelativeFile = function(path) {
-  return fs.readFileSync('.' + path);
-};
-var globResponse = null;
+  return fs.readFileSync('.' + path)
+}
+var globResponse = null
 
 var server = http.createServer(function(req, resp) {
-  var url = req.url;
+  var url = req.url
 
   try {
-    if (isMarkdown.test(url)) {
-      resp.setHeader('Content-Type', 'text/html');
-      if (url.indexOf(filename) >= 0 && globResponse) {
-        globResponse = resp;
+    if (hasMarkdownExtension.test(url)) {
+      resp.setHeader('Content-Type', 'text/html')
+      if (url.indexOf(mainReadme) >= 0 && globResponse) {
+        globResponse = resp
       }
       else {
-        parser(getRelativeFile(url), function(err, result) {
-          if (err) throw err;
-          resp.end(template(result));
-        });
-        globResponse = true;
+        parseMarkdown(getRelativeFile(url).toString(), function(result) {
+          resp.end(template(result))
+        })
+        globResponse = true
       }
     }
     else {
-      resp.end(getRelativeFile(url));
+      resp.end(getRelativeFile(url))
     }
   }
   catch(e) {
-    resp.writeHead(404);
-    resp.end(e.toString());
+    resp.writeHead(404)
+    resp.end(e.toString())
   }
-});
+})
 
-var spawn = require('child_process').spawn;
 server.listen(5678, 'localhost', function(err) {
-  if(err) {
-    throw err;
-  }
-  var host = ['http://localhost:' + server.address().port + '/' + filename];
-  spawn('open', host);
-  console.log('Server started on ' + host.join(''));
-});
+  if (err) throw err
+  var host = 'http://localhost:' + server.address().port + '/' + mainReadme
+  spawn('open', [host])
+  console.log('Server started on ' + host)
+})
 
-var padme = function(num) {
-  return (num < 10) ? '0' + num : num;
+var formatDigit = function(num) {
+  return num < 10 ? '0' + num : num
 }
 
 function watcher(nextPayload) {
-  var moment = nextPayload.mtime;
-
+  var moment = nextPayload.mtime
   var date = [
     moment.getFullYear(),
-    padme(moment.getMonth()),
-    padme(moment.getDay())
-  ].join('/');
-
+    formatDigit(moment.getMonth()),
+    formatDigit(moment.getDay())
+  ].join('/')
   var time = [
-    padme(moment.getHours()),
-    padme(moment.getMinutes()),
-    padme(moment.getSeconds())
-  ].join(':');
+    formatDigit(moment.getHours()),
+    formatDigit(moment.getMinutes()),
+    formatDigit(moment.getSeconds())
+  ].join(':')
+  console.log(date, time, 'Detected file change'.cyan)
 
-  console.log(date, time, 'Detected file change'.cyan);
-  var contents = fs.readFileSync(filename);
-  parser(contents, function(err, result) {
-    if (err) throw err;
-    globResponse.end(template(result));
-  });
+  var contents = fs.readFileSync(mainReadme)
+  parseMarkdown(contents.toString(), function(result) {
+    globResponse.end(template(result))
+  })
 }
 
-fs.watchFile(filename, {
+fs.watchFile(mainReadme, {
   interval: 300
-}, watcher);
+}, watcher)
 
-var closed = false;
+var once = false
 function gracefulServerShutdown() {
-  if (!closed) {
-    closed = true;
-    server.close();
+  if (!once) {
+    console.log('\nreadme-server terminated'.red)
+    once = true
+    server.close()
+    process.exit(0)
   }
-  process.exit(0);
 }
 
 process
   .on('exit', gracefulServerShutdown)
   .on('SIGINT', gracefulServerShutdown)
-  .on('uncaughtException', gracefulServerShutdown);
+  .on('uncaughtException', gracefulServerShutdown)
